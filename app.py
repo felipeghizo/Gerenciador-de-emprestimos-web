d6a7d04019c4fd5dde180e6224e44ae01a1262bd
Felipe Ghizoni <58018357+felipeghizo@users.noreply.github.com>
2024-08-15 10:27:13 -0300

    feat: Mensagem de erro ao efetuar login com dados errados

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for
+import mysql.connector
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    show_error = False
     if request.method == 'POST':
         banco = request.form.get('Banco_connect')
         ip = request.form.get('IP_connect')
         senha = request.form.get('Senha_connect')
-        
-        # Aqui você faria a lógica de conexão com o banco de dados
-        if connect_to_db(banco, ip, senha):  # Supondo que essa função verifique a conexão
+        print(banco, ip, senha)
+        if connect_to_db(banco, ip, senha):  
             return redirect(url_for('envios'))
         else:
-            return "Falha na conexão."
-    return render_template('index.html')
+            show_error = True
+            return render_template('index.html', show_error=show_error)
+    return render_template('index.html', show_error=show_error)
 
 @app.route('/envios')
 def envios():
@@ -33,8 +35,37 @@ def historico():
     return render_template('Historico.html')  
 
 def connect_to_db(banco, ip, senha):
+        # Configurações do banco de dados
+    db_config = {
+        'user': 'seu_usuario',
+        'password': senha,
+        'host': ip,
+        'database': banco
+    }
+    if banco == "envios" and ip == "10.100.68.253" and senha=="Camerasip135.":
+        try: 
+            # Conectar ao banco de dados
+            conn = mysql.connector.connect(**db_config)
+            cursor = conn.cursor(dictionary=True)
 
-    return True
+            # Consulta SQL
+            query = "SELECT clienteid, cameraid, acesso FROM envios"
+            cursor.execute(query)
 
+            # Buscar todos os resultados
+            results = cursor.fetchall()
+
+            # Fechar a conexão
+            cursor.close()
+            conn.close()
+
+            # Renderizar a página HTML com os dados
+            return True
+
+        except mysql.connector.Error as err:
+            return f"Erro: {err}"
+    else:
+        return False
+    
 if __name__ == '__main__':
     app.run(debug=True)
